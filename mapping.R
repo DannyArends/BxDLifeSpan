cb9f78c41c1c5f6da11b48acfae4467245f098fd
Danny Arends <Danny.Arends@gmail.com>
2025-09-30 09:50:53 +0100

    Using linear mixed models for individual level mapping

--- mapping.R
+++ mapping.R
@@ -2,7 +2,7 @@
 # Lifespan - Actuarial mapping Strain means as well as individual level data mapping
 # 3 Month steps (~ 100 days) hit the bodyweight timepoints
 # AgeAtSetUp.in.colony..days. = Day at which the diet switch occured (HFD, as well as NAM), CD didn't switch they ate chow
-
+setwd("C:/Github/BxDLifespan/")
 ldata <- read.table("data/BxD_Lifespan.txt", sep = "\t", header = TRUE, row.names=2)
 bdata <- read.table("data/BxD_Bodyweights.txt", sep = "\t", header = TRUE)
 
@@ -58,24 +58,29 @@ for(diet in c("CD", "HF")){
   plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
 }
 
-
-### individual Level mapping (Approach 1)
-## TODO extend to Actuarial
+library(lme4)
+### individual Level mapping (Random effect)
 op <- par(mfrow = c(2,1))
 for(diet in c("CD", "HF")){
   isCD <- ldata[which(ldata[, "Diet"] == diet & grepl("BXD", ldata[, "StrainName"])),]
   isCD <- isCD[which(isCD[, "StrainName"] %in% colnames(geno)),]
   genoS <- geno[, isCD[, "StrainName"]]
-
-  wF <- 1 / table(isCD[, "StrainName"])
-  weights <- unlist(lapply(isCD[, "StrainName"], function(x){wF[x]}))
-
+  strains <- isCD[, "StrainName"]
+  null <- lmer(isCD[, "AgeAtDeath..days."] ~ 1 + (1 | strains), REML = FALSE)
 
   pvals <- c()
   for(x in c(1:nrow(geno))) {
     gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
-    pvals <- c(pvals, anova(lm(isCD[, "AgeAtDeath..days."] ~ gts, weights = weights))[[5]][1])
-    if(x %% 1000 == 1) cat(x, "\n")
+    full <- lmer(isCD[, "AgeAtDeath..days."] ~ gts + (1 | strains), REML = FALSE)
+    if(any(is.na(gts))){
+      ## If we have missing genotypes, we need to account for this, specify an ALT model
+      alt <- lmer(isCD[which(!is.na(gts)), "AgeAtDeath..days."] ~ 1 + (1 | strains[which(!is.na(gts))]), REML = FALSE)
+      pvals <- c(pvals, as.numeric(na.omit(anova(alt, full)[, "Pr(>Chisq)"])))
+    }else{
+      ## No missing genotypes, so just use the global NULL model which has all data
+      pvals <- c(pvals, as.numeric(na.omit(anova(null, full)[, "Pr(>Chisq)"])))
+    }
+    if(x %% 100 == 1) cat(x, "\n")
   }
   write.table(cbind(map, -log10(pvals)), file = paste0(diet, "_indWeighted.txt"), sep = "\t", quote = FALSE)
   plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
