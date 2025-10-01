b4f0268753a4d638a76b022231c6a2f492acfd8d
DannyArends <Danny.Arends@gmail.com>
2025-10-01 17:42:15 +0100

    Updated for today

--- mapping.R
+++ mapping.R
@@ -36,14 +36,16 @@ geno <- geno[, -c(1:4)]
 rownames(geno) <- map[,2]
 rownames(map) <- map[,2]
 
-### Mapping Strain means
+### Mapping Weighted Strain means
 op <- par(mfrow = c(2,1))
-for(diet in c("CD", "HF")){
+for(diet in c("CD", "HF")) {
   isCD <- ldata[which(ldata[, "Diet"] == diet),]
   mtable <- table(isCD[, "StrainName"])
   mtable <- mtable[which(mtable >= 3)]
   strains <- names(mtable)
   bxds <- strains[grep("BXD", strains)]
+
+  weights <- mtable[bxds]
 
   strainM <- round(unlist(lapply(bxds, function(x){ mean(isCD[which(isCD[, "StrainName"] == x), "AgeAtDeath..days."]); })), 0)
   names(strainM) <- bxds
@@ -53,31 +55,10 @@ for(diet in c("CD", "HF")){
   pvals <- c()
   for(x in c(1:nrow(geno))) {
     gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
-    pvals <- c(pvals, anova(lm(strainM ~ gts))[[5]][1])
+    pvals <- c(pvals, anova(lm(strainM ~ gts, weights = weights))[[5]][1])
     if(x %% 1000 == 1) cat(x, "\n")
   }
   write.table(cbind(map, -log10(pvals)), file = paste0(diet, "_strainMeans.txt"), sep = "\t", quote = FALSE)
-  plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
-}
-
-### individual Level mapping (Wrong: Doesn't deal with correlation structure, as such effects are overestimated due to DoF error)
-op <- par(mfrow = c(2,1))
-for(diet in c("CD", "HF")){
-  isCD <- ldata[which(ldata[, "Diet"] == diet & grepl("BXD", ldata[, "StrainName"])),]
-  isCD <- isCD[which(isCD[, "StrainName"] %in% colnames(geno)),]
-  genoS <- geno[, isCD[, "StrainName"]]
-
-  wF <- 1 / table(isCD[, "StrainName"])
-  weights <- unlist(lapply(isCD[, "StrainName"], function(x){wF[x]}))
-
-
-  pvals <- c()
-  for(x in c(1:nrow(geno))) {
-    gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
-    pvals <- c(pvals, anova(lm(isCD[, "AgeAtDeath..days."] ~ gts, weights = weights))[[5]][1])
-    if(x %% 1000 == 1) cat(x, "\n")
-  }
-  write.table(cbind(map, -log10(pvals)), file = paste0(diet, "_indWeighted_wrong.txt"), sep = "\t", quote = FALSE)
   plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
 }
 
@@ -109,6 +90,61 @@ for(diet in c("CD", "HF")){
 }
 
 ### CTL mapping
+
+### Progressive Mapping
+### Mapping Weighted Strain means
+op <- par(mfrow = c(2,1))
+for(diet in c("HF")){ #c("CD", "HF")) {
+  pvalsM <- c()
+
+  library(parallel)
+  n.cores <- detectCores() - 5
+  clust <- makeCluster(n.cores)
+  clusterExport(clust, "ldata")
+  clusterExport(clust, "diet")
+  clusterExport(clust, "geno")
+
+  pvalsL <- parLapply(clust, seq(30, 900, 60), function(day){
+    isCD <- ldata[which(ldata[, "Diet"] == diet & ldata[, "AgeAtDeath..days."] >= day),]
+    mtable <- table(isCD[, "StrainName"])
+    mtable <- mtable[which(mtable >= 3)]
+    strains <- names(mtable)
+    bxds <- strains[grep("BXD", strains)]
+    weights <- mtable[bxds]
+
+    strainM <- round(unlist(lapply(bxds, function(x){ mean(isCD[which(isCD[, "StrainName"] == x), "AgeAtDeath..days."]); })), 0)
+    names(strainM) <- bxds
+
+    genoS <- geno[,names(strainM)]
+
+    pvals <- c()
+    for(x in c(1:nrow(geno))) {
+      gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
+      pvals <- c(pvals, anova(lm(strainM ~ gts, weights = weights))[[5]][1])
+      if(x %% 1000 == 1) cat(x, "\n")
+    }
+    return(pvals)
+  })
+  plot(-log10(pvalsL[[1]]), col = as.numeric(as.factor(map[, "Chr"])))
+
+  pvalM <- c()
+  for(x in 1:length(pvalsL)){
+    pvalM <- rbind(pvalM, pvalsL[[x]])
+  }
+
+  op <- par(mfrow=c(2,1))
+  image(-log10(t(pvalM[,])), breaks = c(0,1,2,3,4,5,6,10), 
+    col = c("white", "#E1E1E1", "#D0D0D0", "#00C0C0","#00B0B0","#00A0A0","#009090"), xaxt="n", yaxt = "n")
+  axis(2, at = (1:15 - 0.5) / 15, seq(30, 900, 60), las=2)
+  plot(-log10(pvalsL[[1]]), col = as.numeric(as.factor(map[, "Chr"])), xaxs="i")
+
+  write.table(cbind(map, -log10(pvalsM)), file = paste0(diet, "_strainMeans_Progressive.txt"), sep = "\t", quote = FALSE)
+ }
+  #plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
+}
+
+
+
 
 ### Interactions together next Wednesday
 
