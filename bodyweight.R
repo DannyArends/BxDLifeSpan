e19ca84aaa7274595eba53c56da3fec7972160eb
DannyArends <Danny.Arends@gmail.com>
2025-10-20 10:20:09 +0100

    Progressive mapping of strain means per diet

--- bodyweight.R
+++ bodyweight.R
@@ -59,42 +59,68 @@ for(x in good){
   xe <- which(colnames(BWs) == tp.max)
 
   res.a <- approx(tps, y, as.numeric(colnames(BWs)[xs:xe]))
-
-  plot(res.a$x, res.a$y); points(tps, y, col = "red", pch = 16)
+  plot(c(0, 1200), c(0, 120), t = "n")
+  points(res.a$x, res.a$y); points(tps, y, col = "red", pch = 16)
   BWs[x, as.character(res.a$x)] <- round(res.a$y,1)
   cat(x, "[",length(which(!is.na(BWs[x,]))),"]: ", tp.min,"-", tp.max, "\n")
-
 }
 
-plot(as.numeric(colnames(BWs)), nrow(BWs) - apply(apply(BWs, 2, is.na),2,sum), main = "Sample Size")
+# Subset to only the BWs animals
+bdata <- bdata[rownames(BWs),]
 
-diet <- bdata[rownames(BWs), "Diet"]
+# Sample sizes
+diet <- bdata[, "Diet"]
+CD <- which(diet == "CD")
+HF <- which(diet == "HF")
+nSamples <- nrow(BWs) - apply(apply(BWs, 2, is.na),2,sum)
+n.CD <- length(CD) - apply(apply(BWs[CD,], 2, is.na),2,sum)
+n.HF <- length(HF) - apply(apply(BWs[HF,], 2, is.na),2,sum)
 
+timepoints <- as.numeric(colnames(BWs))
+plot(timepoints, nSamples, main = "Sample Size", pch = 19, ylab = "nSamples", xlab = "Time")
+points(timepoints, n.CD, pch = 19, col = rgb(1,0,1,0.5))
+points(timepoints, n.HF, pch = 19, col = rgb(0,1,1,0.5))
+legend("topright", c("All", "CD", "HF"), pch = 19, col = c(1, rgb(1,0,1,0.5),  rgb(0,1,1,0.5)))
+
+# Reasonable timepoints
+mapAble <- names(which(nSamples > 400))
+tpsA <- mapAble[which(mapAble %in% as.character(seq(30, 780, 30)))]
 
 ### Mapping Weighted Strain means
-op <- par(mfrow = c(2,1))
+
 for(diet in c("CD", "HF")) {
-  isDIET <- bdata[which(ldata[, "Diet"] == diet),]
-  mtable <- table(isDIET[, "StrainName"])
-  mtable <- mtable[which(mtable >= 3)]
-  strains <- names(mtable)
-  bxds <- strains[grep("BXD", strains)]
+  pvalsM <- c()
+  op <- par(mfrow = c(2,1))
+  for(tp in tpsA){
+    # Animals on the diet with bodyweight available
+    isDIET <- bdata[which(bdata[, "Diet"] == diet & !is.na(BWs[,tp])),]
+    mtable <- table(isDIET[, "StrainName"])
+    mtable <- mtable[which(mtable >= 3)]
+    strains <- names(mtable)
+    bxds <- strains[grep("BXD", strains)]
+    weights <- mtable[bxds]
 
-  weights <- mtable[bxds]
+    # Compute strain means for the strains that have at least 3 animals
+    strainM <- round(unlist(lapply(bxds, function(x){
+      isStrain <- rownames(isDIET[which(isDIET[, "StrainName"] == x),])
+      return(mean(BWs[isStrain, tp], na.rm = TRUE))
+    })), 1)
+    names(strainM) <- bxds
 
-  strainM <- round(unlist(lapply(bxds, function(x){ 
-    mean(isDIET[which(isDIET[, "StrainName"] == x), "AgeAtDeath..days."]); })), 2)
-  names(strainM) <- bxds
+    genoS <- geno[,names(strainM)]
 
-  genoS <- geno[,names(strainM)]
-
-  pvals <- c()
-  for(x in c(1:nrow(geno))) {
-    gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
-    pvals <- c(pvals, anova(lm(strainM ~ gts, weights = weights))[[5]][1])
-    if(x %% 1000 == 1) cat(x, "\n")
+    # Mapping
+    pvals <- c()
+    for(x in c(1:nrow(geno))) {
+      gts <- as.numeric(factor(as.character(genoS[x, ]), levels = c("B", "H", "D"))) - 2
+      pvals <- c(pvals, anova(lm(strainM ~ gts, weights = weights))[[5]][1])
+      if(x %% 1000 == 1) cat(x, "\n")
+    }
+    plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])), main = paste0(diet, "@", tp, ",n=", length(bxds)))
+    pvalsM <- cbind(pvalsM, pvals)
   }
-  write.table(cbind(map, -log10(pvals)), file = paste0("output/", diet, "_wMeans.txt"), sep = "\t", quote = FALSE)
-  plot(-log10(pvals), col = as.numeric(as.factor(map[, "Chr"])))
+  res <- cbind(map, -log10(pvalsM))
+  colnames(res) <- c("Chr","Locus","cM","Mb", tpsA)
+  write.table(res, file = paste0("output/BW_", diet, "_wMeans.txt"), sep = "\t", quote = FALSE)
 }
 
